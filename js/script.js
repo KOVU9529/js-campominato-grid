8faf8803941e9e9aac9ce9327f080884eb5df3a2
KOVU9529 <bapgiordano@gmail.com>
2022-06-14 18:14:19 +0200

    lavoro ultimato

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@
 2-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
   I numeri nella lista delle bombe non possono essere duplicati.
 ---------------------------------------------------------------------------------------
-In seguito attraverso dei prompt l'utente inserisce un numero alla volta finche il gioco non è finito:
-se il numero è presente nella lista dei numeri generati, abbiamo calpestato una bomba, il gioco finisce con un messaggio di errore
-Altrimenti il gioco va avanti a meno di aver raggiunto il numero massimo di tentativi possibili. In questo caso il gioco finisce con un messaggio di vittoria.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha scelto un numero che non era una bomba.*/
+3-In seguito attraverso dei prompt l'utente inserisce un numero alla volta finche il gioco non è finito:
+  se il numero è presente nella lista dei numeri generati, abbiamo calpestato una bomba, il gioco finisce con un messaggio di errore
+  Altrimenti il gioco va avanti a meno di aver raggiunto il numero massimo di tentativi possibili. In questo caso il gioco finisce con un messaggio di vittoria.
+---------------------------------------------------------------------------------------
+4-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha scelto un numero che non era una bomba.*/
 
+//1-
 //Chiedo all'utente la difficiltà (attraverso la scelta di un numero)
 const userLevel=parseInt(prompt('Dimmi un numero da 1 a 3 (Più alto è il valore del numero scelto maggiore sarà la difficoltà)'));
 
@@ -33,6 +35,7 @@ switch(userLevel){
 //Testo il funzionamento
 console.log(typeLevel);
 
+//2-
 //Numero delle bombe
 const bombNumb=16;
 //Valore della funzione esterno alla funzione stessa
@@ -40,7 +43,57 @@ const generateBombArray=generateBomb(bombNumb,1,typeLevel);
 //controllo
 console.log(generateBombArray);
 
+//3-
+//Inserimento dei numeri
+//Creo un array vuoto(che verra popolato con la seconda scelta numerica)
+const arrayEmpt=[];
 
+//Calcolo il massimale dei tentativi in funzione della difficoltà
+const attempts = typeLevel-bombNumb;
+
+//Prima di inizializzare il ciclo imposto la mia variabile
+let gameResult=true;
+
+//Tramite il ciclo while imposto le condizioni delle aggiunte
+while(gameResult){
+    //seconda scelta numerica
+    const userAttempts=parseInt(prompt('Dimmi un numero'));
+
+    //imposto le condizioni delle aggiunte
+    if (generateBombArray.includes(userAttempts)){
+        gameResult=false;
+        alert('Peccato hai perso!');
+        alert('Tentativi riusciti:'+ arrayEmpt)
+    }else{
+        if(!generateBombArray.includes(userAttempts)){
+            arrayEmpt.push(userAttempts);
+        } if (arrayEmpt.length===attempts){
+            gameResult=false;
+            alert('Complimenti hai vinto!')
+        }
+    }
+}
+
+
+//--------------------------
+//Function
+//--------------------------
+
+//4-
+//Funzione per generare il vincitore e i temtativi andati a buon fine
+//1-risultati gioco
+//2-Lunghezza array=numero di tentativi
+
+function endGame(gameResults, arrayMatches){
+    if(gameResults==='Hai vinto'){
+        alert('Complimenti hai vinto!')
+    } else if(gameResults==='Hai perso'){
+        alert('Peccato hai perso!');
+        alert('Tentativi riusciti:'+arrayMatches.lenght)
+    }
+}
+
+//2-
 //Funzione per il popolamento dell'array(contenitore delle bombe)
 //bombElement-> popoleranno bombArray
 //imposto come due ultimi input il min e max
@@ -59,6 +112,7 @@ function generateBomb(bombElement,rangeMin,rangeMax){
     return bombArray;
 }
 
+//2-
 //Genero una funzione per i 16 numeri random
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
